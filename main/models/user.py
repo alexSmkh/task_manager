ef453e64b43fe2921f7cd942a87915a66028d047
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-04-11 18:22:14 +0300

    feature: add roles to users

--- main/models/user.py
+++ main/models/user.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+
+    class Roles(models.TextChoices):
+        DEVELOPER = "developer"
+        MANAGER = "manager"
+        ADMIN = "admin"
+
+    role = models.CharField(max_length=255, choices=Roles.choices, default=Roles.DEVELOPER)
