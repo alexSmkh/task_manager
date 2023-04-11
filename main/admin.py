e181d3c0db32e9ee4c0bf4f6ea3f2a5a68f0e85b
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-04-12 00:44:38 +0300

    feature: customize admin site

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
+
+from main.models import Tag, Task
 from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class TaskManagerAdminSite(admin.AdminSite):
+    pass
+
+
+task_manager_admin_site = TaskManagerAdminSite(name="Task manager admin")
+
+
+@admin.register(Tag, site=task_manager_admin_site)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task, site=task_manager_admin_site)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+task_manager_admin_site.register(User, UserAdmin)
